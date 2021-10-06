f94062806def386c55364c470310df2547c77c0d
kotan.kn <1373625+kotan-kn@users.noreply.github.com>
2021-10-06 15:02:50 +0900

    add all files

---
+++ DNSR/DNSRApp.swift
@@ -0,0 +1,15 @@
+//
+//  DNSRApp.swift
+//  DNSR
+//
+//  Created by Kota Nakano on 10/6/21.
+//
+import SwiftUI
+@main
+struct DNSRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
