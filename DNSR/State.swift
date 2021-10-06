0a138821b4c44bb5e020e1f0b798431dd9d4ac8f
kotan.kn <1373625+kotan-kn@users.noreply.github.com>
2021-10-06 20:14:56 +0900

    states

--- DNSR/State.swift
+++ DNSR/State.swift
@@ -7,11 +7,24 @@
 import SwiftUI
 typealias State = UserDefaults
 extension State {
+	func getHost() -> String {
+		string(forKey: "host") ?? ""
+	}
+	func set(host: String) {
+		setValue(host, forKey: "host")
+		synchronize()
+	}
+	var host: Binding<String> {
+		.init(get: getHost, set: set(host:))
+	}
+}
+extension State {
 	func getUser() -> String {
 		string(forKey: "user") ?? ""
 	}
 	func set(user: String) {
 		set(user, forKey: "user")
+		synchronize()
 	}
 	var user: Binding<String> {
 		.init(get: getUser, set: set(user:))
@@ -23,9 +36,22 @@ extension State {
 	}
 	func set(pass: String) {
 		set(pass, forKey: "pass")
+		synchronize()
 	}
 	var pass: Binding<String> {
 		.init(get: getPass, set: set(pass:))
+	}
+}
+extension State {
+	func getStatus() -> String {
+		string(forKey: "status") ?? ""
+	}
+	func set(status: String) {
+		setValue(status, forKey: "status")
+		synchronize()
+	}
+	var status: Binding<String> {
+		.init(get: getStatus, set: set(status:))
 	}
 }
 extension State: ObservableObject {
