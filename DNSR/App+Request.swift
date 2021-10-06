6ef2b0684063eaa8f49ba1bc7e3fd3d08a7fb217
kotan.kn <1373625+kotan-kn@users.noreply.github.com>
2021-10-07 03:11:47 +0900

    simplify

--- DNSR/App+Request.swift
+++ DNSR/App+Request.swift
@@ -8,16 +8,15 @@ import Foundation
 import Combine
 extension App {
 	func request(host: String, user: String, pass: String) -> AnyPublisher<String, URLSession.DataTaskPublisher.Failure> {
-		let session: URLSession = .shared
-		let resolve = URL(string: "https://domains.google.com/checkip").publisher.flatMap(session.dataTaskPublisher).flatMap {(data, response) -> Optional<String>.Publisher in
+		func parse(data: Data, response: URLResponse) -> Optional<String>.Publisher {
 			switch response {
 				case let response as HTTPURLResponse where (200..<300).contains(response.statusCode):
 					return String(data: data, encoding: .utf8).publisher
 				default:
-					return Optional<String>.none.publisher
+					return.init(.none)
 			}
-			}.eraseToAnyPublisher()
-		let query = resolve.flatMap { address -> Optional<URL>.Publisher in
+		}
+		func compose(address: String) -> Optional<URL>.Publisher {
 			var components = URLComponents()
 			components.scheme = "https"
 			components.host = "domains.google.com"
@@ -25,18 +24,18 @@ extension App {
 			components.user = user
 			components.password = pass
 			components.queryItems = [
-				URLQueryItem(name: "hostname", value: "proxy.xsgn.art"),
+				URLQueryItem(name: "hostname", value: host),
 				URLQueryItem(name: "myip", value: address),
 			]
 			return components.url.publisher
 		}
-		return query.flatMap(session.dataTaskPublisher).flatMap {(data, response) -> Optional<String>.Publisher in
-			switch response {
-				case let response as HTTPURLResponse where (200..<300).contains(response.statusCode):
-					return String(data: data, encoding: .utf8).publisher
-				default:
-					return Optional<String>.none.publisher
-			}
-		}.eraseToAnyPublisher()
+		let session: URLSession = .shared
+		return URL(string: "https://domains.google.com/checkip").publisher
+			.flatMap(session.dataTaskPublisher)
+			.flatMap(parse)
+			.flatMap(compose)
+			.flatMap(session.dataTaskPublisher)
+			.flatMap(parse)
+			.eraseToAnyPublisher()
 	}
 }
