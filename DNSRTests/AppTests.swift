f8c11df34c800d30556d0eb405a237dd08266885
kotan.kn <1373625+kotan-kn@users.noreply.github.com>
2021-10-07 16:17:29 +0900

    tests

---
+++ DNSRTests/AppTests.swift
@@ -0,0 +1,16 @@
+//
+//  AppTests.swift
+//  DNSRTests
+//
+//  Created by Kota Nakano on 10/7/21.
+//
+import XCTest
+@testable import DNSR
+class AppTests: XCTestCase {
+	let app = App()
+	var cancel: Any?
+}
+extension AppTests {
+	func testRequestSuccess() {
+	}
+}
